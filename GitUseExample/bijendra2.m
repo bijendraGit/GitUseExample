a095367c9f4dd7acb1669296315d05215d10ac6d
Cyberlinks <cyberlinks@bijendras-mac-mini.local>
2016-03-04 17:26:06 +0530

    replace code of New Branch by Bijendra 2

---
+++ GitUseExample/bijendra2.m
@@ -0,0 +1,13 @@
+//
+//  bijendra2.m
+//  GitUseExample
+//
+//  Created by Cyberlinks on 04/03/16.
+//  Copyright © 2016 Cyberlinks. All rights reserved.
+//
+
+#import "bijendra2.h"
+
+@implementation bijendra2
+
+@end
---
+++ GitUseExample/bijendra22.m
@@ -0,0 +1,13 @@
+//
+//  bijendra22.m
+//  GitUseExample
+//
+//  Created by Cyberlinks on 04/03/16.
+//  Copyright © 2016 Cyberlinks. All rights reserved.
+//
+
+#import "bijendra22.h"
+
+@implementation bijendra22
+
+@end
